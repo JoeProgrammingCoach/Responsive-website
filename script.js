52462c67710cbbc17c73a3c9f1758b9849ee1590
JoeProgrammingCoach <117598378+JoeProgrammingCoach@users.noreply.github.com>
2023-07-16 15:50:41 -0400

    have restructured my html, css and javascript files

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const nav = document.querySelector(".fixedLeftNav")
+const nav = document.querySelector(".openLeftNav")
+const closedNav = document.querySelector(".closedLeftNav")
+const leftNav = document.querySelector(".leftNav")
+
+const hidden = document.querySelector('.hidden')
+
+
+closedNav.addEventListener('click', () => {
+hidden.classList.toggle('hidden')
+leftNav.classList.toggle("openLeftNav")
+leftNav.classList.toggle("closedLeftNav")
+
+console.log("element was clicked!")
+})
+
